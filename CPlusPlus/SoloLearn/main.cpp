32401ae9d70da2dbf2d85fbc40aa27ac1ba9b9c9
Kris Johnson <kris.jhnsn@gmail.com>
2019-02-04 15:32:05 -0500

    adding a notes file, cout, cin, basic variables, comments

--- CPlusPlus/SoloLearn/main.cpp
+++ CPlusPlus/SoloLearn/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main()
 {
+    // single line comment
+    /* blockline
+    comment */
     cout << "Hello world!" << endl;
+    int a,b,num; // valid declaration
+    cout << "Please input an int\n";
+    cin >> num;
+    a = num; b = 20;
+    int sum = a+b;
+    cout << "Your int plus 20 is:" << endl;
+    cout << sum << endl;
     return 0;
 }
