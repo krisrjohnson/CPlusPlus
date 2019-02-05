602f08f4311802f2378c1baaeb2d9e5476d8ebfc
Kris Johnson <kris.jhnsn@gmail.com>
2019-02-04 22:24:41 -0500

    data types, pointers, dereferencing, intro to memory alloc, fns, sizeof(), arrays, for loops, if, else, switch, while, do..while

--- CPlusPlus/SoloLearn/main.cpp
+++ CPlusPlus/SoloLearn/main.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+void printSomething(); // declare the fn so can be used in main()
+
 int main()
 {
     // single line comment
@@ -9,11 +11,30 @@ int main()
     comment */
     cout << "Hello world!" << endl;
     int a,b,num; // valid declaration
+    int *pnum = &num; // pointer declaration
     cout << "Please input an int\n";
     cin >> num;
-    a = num; b = 20;
-    int sum = a+b;
-    cout << "Your int plus 20 is:" << endl;
-    cout << sum << endl;
+    cout << "the memory address is " << endl << pnum << endl;
+    cout << "the num you input based on that address is " << *pnum << endl;
+    cout << "size of double: " << sizeof(double) << endl;
+    printSomething();
     return 0;
 }
+
+void printSomething() {
+    cout << "Hi there!" << endl;
+}
+
+/* cout, cin, int, float, double, long, signed, unsigned
+    do {} while();
+    while() {stmts; i++;};
+    switch(expression){
+    case1: stmts; break;
+    case2: stmts;
+    case3; stmts; break // if satisfies case2, will test against case3 as well
+    default: stmts;}
+    for (int i = 0; i<10; i++) {stmts}
+    &&, ||, !=, ==, !
+    int *ip; // integer pointer declaration
+
+*/
