f34a7e6bbf604f15e63dddd066e3dd50f984bea9
Kris Johnson <kris.jhnsn@gmail.com>
2019-02-10 23:24:02 -0500

    updated notes and main.cpp for overloading, recursion, and passing args to fns by reference (&x vs just x)

--- CPlusPlus/SoloLearn/main.cpp
+++ CPlusPlus/SoloLearn/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-void printSomething(); // declare the fn so can be used in main()
+void printSomething(string); // declare the fn so can be used in main()
+void printSomething(int);
+int fact(int);
+void myFunc(int*);
 
 int main()
 {
@@ -10,21 +13,42 @@ int main()
     /* blockline
     comment */
     cout << "Hello world!" << endl;
-    int a,b,num; // valid declaration
+    int x,a,b,num; // valid declaration
     int *pnum = &num; // pointer declaration
     cout << "Please input an int\n";
     cin >> num;
     cout << "the memory address is " << endl << pnum << endl;
     cout << "the num you input based on that address is " << *pnum << endl;
     cout << "size of double: " << sizeof(double) << endl;
-    printSomething();
+    printSomething("Hello!");
+    printSomething(fact(5));
+
+    // testing passing memory addresses to functions
+    x = num;
+    cout << "your number was: ";
+    printSomething(x);
+    myFunc(&x);
+    cout << "your new number is: ";
+    printSomething(x);
     return 0;
 }
-
-void printSomething() {
-    cout << "Hi there!" << endl;
+void myFunc(int *x) {
+    *x = 100;
 }
 
+void printSomething(string straw="Hi there!") {
+    cout << straw << endl;
+}
+void printSomething(int straw=4){
+    cout << straw << endl;
+}
+
+int fact(int n) {
+    if (n==1) {return n;}
+    else {
+        return n * fact(n-1);  // recursion!
+    }
+}
 /* cout, cin, int, float, double, long, signed, unsigned
     do {} while();
     while() {stmts; i++;};
